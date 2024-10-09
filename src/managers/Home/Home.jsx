1c9c8dc69651606e376800a22e241130473e20d8
ayushgit12 <ayush2005raj@gmail.com>
2024-10-09 17:33:00 +0530

    shuruat

---
+++ src/managers/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home1 from './Home1/Home1'
+
+const Home = () => {
+  return (
+    <div className='mt-[48px] mx-24'>
+      <Home1 />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/managers/Home/Home1/Home1.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import styles from './Frame.module.css';
+import search from "../../../assets/search.svg";
+import home1 from "../../../assets/home1.svg";
+
+const Home1 = () => {
+  return (
+    <div style={{scale: "90%"}} className={styles.frameParent}>
+      <div className={styles.frameGroup}>
+        <div className={styles.frameContainer}>
+          <div className={styles.frameContainer}>
+            <div className={styles.modernizingYourJobSearchExParent}>
+              <b className={styles.modernizingYourJobContainer}>
+                <p className={styles.modernizingYour}>Modernizing your</p>
+                <p className={styles.modernizingYour}>
+                  <span className={styles.jobSearch}>Job search</span>
+                  <span> Experience</span>
+                </p>
+              </b>
+              <div className={styles.over100000JobsContainer}>
+                <p
+                  className={styles.modernizingYour}
+                >{`Over 1,00,000+ jobs listed here and over `}</p>
+                <p className={styles.modernizingYour}>
+                  20,000 students have registered for projects
+                </p>
+              </div>
+            </div>
+            <div className={styles.frameParent1}>
+              <div className={styles.bxsearchParent}>
+                <img
+                  className={styles.bxsearchIcon}
+                  alt=""
+                  src={search}
+                />
+                <div className={styles.searchForYour}>
+                  Search for your dream project
+                </div>
+              </div>
+              <div className={styles.searchWrapper}>
+                <div className={styles.searchForYour}>Search</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.popularProjectsYouCanStartParent}>
+            <div className={styles.popularProjectsYou}>
+              Popular projects you can start on :
+            </div>
+            <div className={styles.frameParent2}>
+              <div className={styles.uiuxWrapper}>
+                <div className={styles.searchForYour}>UI/UX</div>
+              </div>
+              <div className={styles.uiuxWrapper}>
+                <div className={styles.searchForYour}>Web Development</div>
+              </div>
+              <div className={styles.uiuxWrapper}>
+                <div className={styles.searchForYour}>E-commerce</div>
+              </div>
+              <div className={styles.uiuxWrapper}>
+                <div className={styles.searchForYour}>Health and Wellness</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className={styles.homepageHeroSectionImagePnIcon}
+          alt=""
+          src={home1}
+        />
+      </div>
+      <div className={styles.frameParent3}>
+        <div className={styles.frameParent4}>
+          <div className={styles.frameChild} />
+          <div className={styles.parent}>
+            <b className={styles.b}>500 +</b>
+            <div className={styles.talent}>TALENT</div>
+          </div>
+        </div>
+        <div className={styles.frameParent4}>
+          <div className={styles.frameChild} />
+          <div className={styles.group}>
+            <b className={styles.b1}>25+</b>
+            <div className={styles.talent}>TEAMS</div>
+          </div>
+        </div>
+        <div className={styles.frameParent4}>
+          <div className={styles.frameChild} />
+          <div className={styles.group}>
+            <b className={styles.b1}>15+</b>
+            <div className={styles.talent}>UNIVERSITIES</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home1;
