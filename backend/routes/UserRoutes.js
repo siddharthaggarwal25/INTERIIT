379227fab62f886c1e0195aabec82d6bf4f9c72f
siddharthaggarwal25 <aggarwalsiddharth49@gmail.com>
2024-10-11 13:15:42 +0530

    added

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,7 +4,7 @@ const  userControllers = require ("../controllers/userControllers")
 
 router.post( "/signup" , userControllers.signup )
 router.post( "/login" , userControllers.login)
-router.post ( "/:UserID/onboarding" , userControllers.onbording)
+router.post ( "/onboarding /:userId" , userControllers.onbording)
  // in requserid add model id of that obejct \
 
  module.exports = router
